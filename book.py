87267cba5adc66a5d6d378d4387bf2a915a16b2a
Pemidi <rashidi.peyman@gmail.com>
2020-11-03 03:11:10 +0330

    search method added to Book model

--- book.py
+++ book.py
@@ -1,14 +1,23 @@
 
-
 class Book:
     """
     a class to initiate books
     """
     book_list = []
 
-    def __init__(self, name, author, price, publish_date):
+    def __init__(self, name, author, price, year_published):
         self.name = name
         self.author = author
         self.price = price
-        self.publish_date = publish_date
+        self.publish_date = year_published
         Book.book_list.append(self)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def search(cls, book_name):
+        for book in cls.book_list:
+            if book.name == book_name:
+                return True
+        return False
